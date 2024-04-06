4e2f3288f9e2e79da8f0b2f05a469ee60f9f110b
petchgamer2544 <petchgamer2544@gmail.com>
2024-04-06 19:36:13 +0700

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,12 +10,14 @@ import { UserRegisterComponent } from './user-register/userRegister/user-registe
 import { SummaryComponent } from './summary/summary/summary.component';
 import { SuccesspageComponent } from './successpage/successpage/successpage.component';
 import { authGuard } from './auth.guard';
+import { rolesGuard } from './roles.guard';
 
 
 const routes: Routes = [
   {
     path: 'detail', loadChildren: () =>
-      import('./detail/detail.module').then(min => min.DetailModule),component: DetailComponent
+      import('./detail/detail.module').then(min => min.DetailModule),component: DetailComponent,
+    canActivate: [authGuard]
   },
   {
     path: 'history', loadChildren: () =>
@@ -24,12 +26,26 @@ const routes: Routes = [
   },
   {
     path: 'upload', loadChildren: () =>
-      import('./upload/upload.module').then(min => min.UploadModule),component: UploadComponent
+      import('./upload/upload.module').then(min => min.UploadModule),component: UploadComponent,
+    canActivate: [authGuard]
   },
   {
     path: 'expresspage', loadChildren: () =>
-      import('./express/express.module').then(min => min.ExpressModule),component: ExpressComponent
+      import('./express/express.module').then(min => min.ExpressModule),component: ExpressComponent,
+    canActivate: [authGuard]
   },
+  // page for admin
+  {
+    path: 'singup', loadChildren: () =>
+      import('./user-register/user-register.module').then(min => min.UserRegisterModule),component: UserRegisterComponent,
+    canActivate: [authGuard, rolesGuard]
+  },
+  {
+    path: 'summary', loadChildren: () =>
+      import('./summary/summary.module').then(min => min.SummaryModule),component: SummaryComponent,
+    canActivate: [authGuard, rolesGuard]
+  },
+  // login line & user login
   {
     path: 'linelogin', loadChildren: () =>
       import('./login/login.module').then(min => min.LoginModule),component: LoginComponent
@@ -38,14 +54,7 @@ const routes: Routes = [
     path: '', loadChildren: () =>
       import('./user-login/user-login.module').then(min => min.UserLoginModule),component: UserLoginComponent
   },
-  {
-    path: 'singup', loadChildren: () =>
-      import('./user-register/user-register.module').then(min => min.UserRegisterModule),component: UserRegisterComponent
-  },
-  {
-    path: 'summary', loadChildren: () =>
-      import('./summary/summary.module').then(min => min.SummaryModule),component: SummaryComponent
-  },
+  // page line login success
   {
     path: 'success', loadChildren: () =>
       import('./successpage/successpage.module').then(min => min.SuccesspageModule),component: SuccesspageComponent
